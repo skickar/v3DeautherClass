3f101ca3b1bcaa4fe359e8f07dbcfc926fa3898b
Skickar <40251293+skickar@users.noreply.github.com>
2020-06-26 12:40:08 -0600

    Update beaconwrapper.py

--- beaconwrapper.py
+++ beaconwrapper.py
@@ -1,4 +1,4 @@
-path = "/Users/skickar/ssids.txt"; ssids = open(path,'r'); command = "beacon "
+path = input("What is the list of SSID's the broadcast?\n>"); ssids = open(path,'r'); command = "beacon "
 for line in ssids:
     command = command +  "\"{}\",".format(line.strip())
 print(command+" -mon")
