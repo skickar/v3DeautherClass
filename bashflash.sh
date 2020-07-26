a2ec19a66c46f4c678091d13db18972ff0c594e3
Skickar <40251293+skickar@users.noreply.github.com>
2020-07-26 17:16:13 -0600

    erase and flash deauther, then check with screen

--- bashflash.sh
+++ bashflash.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-esptool.py --port /dev/cu.wchusbserial14130 erase_flash
+
+##to use, run flashbash.sh with two arguments:
+##1) the location of the serial port of the device you want to flash
+##2) the .bin file you want to flash to the device
+## the script will erase the device, flash to it, 
+## and then try connecting via screen.
+##
+## made for the esp8266 deauther by Skickar
+
+## example run:
+## bash bashflash.sh /dev/ttyUSB0 /Users/skickar/Desktop/dstike.bin
+
+
+esptool.py --port $1 erase_flash
 wait
-esptool.py --port /dev/cu.wchusbserial14130 --baud 460800 write_flash --flash_size=detect 0 /Users/skickar/Desktop/esp_duck.ino.dstike.bin
+esptool.py --port $1 --baud 460800 write_flash --flash_size=detect 0 $2
 wait
 echo "All done, next"
 sleep 5
-screen /dev/cu.wchusbserial14130 115200
+screen $1 115200
